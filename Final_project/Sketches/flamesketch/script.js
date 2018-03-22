42b1db80661a03e638f347d850fbd787c185f2c0
akarakitsos <37158865+akarakitsos@users.noreply.github.com>
2018-03-22 14:31:14 +0100

    removing and adding

--- Final_project/Sketches/flamesketch/script.js
+++ Final_project/Sketches/flamesketch/script.js
@@ -12,22 +12,3 @@ function ready() {
   // Received a message
   socket.onmessage = function(evt) {
     
-//*----CODE----*\\
-
-function fadePics(){
-  var i;
-  var x=  document.getElementsByClassName ("fadePics");
- for(i=0;i<x.length;i++){
- x[i].style.opacity = "0";
- }
- imgIndex++;
- if(imgIndex==x.length-1){
- imgIndex = 0;
- }
- 
- x[imgIndex].style.opacity = "1";
- 
- setTimeout(fadePics,1000);
- 
-}
-fadePics();
